a373a2131b34a9fe2ec0c665d83fe7c96fb83034
YousefAlamir <uoshy2016@gmail.com>
2024-12-02 14:57:30 +0300

    Added: Delete Action in SectionResource.

--- app/Filament/Resources/SectionResource.php
+++ app/Filament/Resources/SectionResource.php
@@ -119,6 +119,7 @@ class SectionResource extends Resource
             ->actions([
                 Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
