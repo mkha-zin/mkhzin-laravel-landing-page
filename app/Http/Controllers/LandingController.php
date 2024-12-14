224f7a91557ee53cb9efa27e7c05de55303ca066
YousefAlamir <uoshy2016@gmail.com>
2024-12-14 11:08:49 +0300

    Fixed: Exporting Data success.

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -267,7 +267,7 @@ class LandingController extends Controller
 
     public function download()
     {
-        $filePath = asset("public/storage/filament_exports/". request()->key . "/" . request()->record);
+        $filePath = public_path("storage/filament_exports/". request()->key . "/" . request()->record);
 
         dd($filePath . " - " . file_exists($filePath));
         if (!file_exists($filePath)) {
