2a850c5916eea174fc0071e846befc23a223954a
YousefAlamir <uoshy2016@gmail.com>
2024-12-24 13:33:22 +0300

    Added: Vouchers table tab filter.

--- app/Filament/Resources/VoucherResource/Pages/ListVouchers.php
+++ app/Filament/Resources/VoucherResource/Pages/ListVouchers.php
@@ -7,18 +7,12 @@ use App\Models\Voucher;
 use Filament\Actions;
 use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Support\Colors\Color;
 use Illuminate\Database\Eloquent\Builder;
 
 class ListVouchers extends ListRecords
 {
     protected static string $resource = VoucherResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            Actions\CreateAction::make(),
-        ];
-    }
 
     public function getTabs(): array
     {
@@ -28,7 +22,8 @@ class ListVouchers extends ListRecords
                 ->extraAttributes([
                     'style' => 'min-width: 140px',
                 ])
-                ->badge(Voucher::all()->count()),
+                ->badge(Voucher::all()->count())
+                ->badgeColor(Color::Blue),
             'Used' => Tab::make()
                 ->label(__('dashboard.used vouchers'))
                 ->extraAttributes([
@@ -37,7 +32,8 @@ class ListVouchers extends ListRecords
                 ->modifyQueryUsing(
                     fn(Builder $query) => $query
                         ->where('used', true))
-                ->badge(Voucher::query()->where('used', true)->count()),
+                ->badge(Voucher::query()->where('used', true)->count())
+                ->badgeColor(Color::Red),
             'UnUsed' => Tab::make()
                 ->label(__('dashboard.unused vouchers'))
                 ->extraAttributes([
@@ -46,16 +42,16 @@ class ListVouchers extends ListRecords
                 ->modifyQueryUsing(
                     fn(Builder $query) => $query
                         ->where('used', false))
-                ->badge(Voucher::query()->where('used', false)->count()),
+                ->badge(Voucher::query()->where('used', false)->count())
+                ->badgeColor(Color::Green),
+
         ];
     }
 
-    /*protected function getTableFilters(): array
+    protected function getHeaderActions(): array
     {
         return [
-            Filter::make('used')
-                ->label(__('dashboard.used vouchers'))
+            Actions\CreateAction::make(),
         ];
-    }*/
-
+    }
 }
