9dedb81135735b49511c7740af9671248de07993
YousefAlamir <uoshy2016@gmail.com>
2025-01-30 10:43:42 +0300

    Enhanced: Store Landing page route.

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -424,7 +424,7 @@
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="{{ url('mkhazin-store') }}" class="nav-link {{ request()->segment(1) == 'store' ? 'active' : '' }}">
+                            <a href="{{ url('estore') }}" class="nav-link {{ request()->segment(1) == 'store' ? 'active' : '' }}">
                                 {{  __('landing.Store')}}
                             </a>
                         </li>
