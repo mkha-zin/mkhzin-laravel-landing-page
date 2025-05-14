b3dacd51f8c463f4e3d24385b3a95d533ed4b84e
YousefAlamir <uoshy2016@gmail.com>
2025-05-14 08:38:31 +0300

    Added: Blog enhanced

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -71,7 +71,7 @@
                                             </figure>
                                             <div class="card-body bg-white p-4 {{ $textAlignment }}">
                                                 <div class="entry-header mb-1">
-                                                    <span class="badge bg-light text-danger">
+                                                    <span class="badge text-danger" style="background-color: #ffeeee">
                                                         {{ $lang == 'en' ? $post->tag->tag_en : $post->tag->tag_ar }}
                                                     </span>
                                                     <h4 class="card-title">
@@ -99,10 +99,8 @@
                                                                  height="14"
                                                                  fill="currentColor" class="bi bi-calendar3"
                                                                  viewBox="0 0 16 16">
-                                                                <path
-                                                                    d="M14 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM1 3.857C1 3.384 1.448 3 2 3h12c.552 0 1 .384 1 .857v10.286c0 .473-.448.857-1 .857H2c-.552 0-1-.384-1-.857V3.857z"/>
-                                                                <path
-                                                                    d="M6.5 7a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+                                                                <path d="M14 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM1 3.857C1 3.384 1.448 3 2 3h12c.552 0 1 .384 1 .857v10.286c0 .473-.448.857-1 .857H2c-.552 0-1-.384-1-.857V3.857z"/>
+                                                                <path d="M6.5 7a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
                                                             </svg>
                                                             <span class="mx-2 fx-7">
                                                             {{ $post->created_at->translatedFormat($lang == 'ar' ? 'd F Y' : 'M d Y') }}
