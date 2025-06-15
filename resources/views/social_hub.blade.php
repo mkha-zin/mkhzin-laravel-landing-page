695babefc7c78293a6e9aeb2c4b0a9bba710a1d4
YousefAlamir <uoshy2016@gmail.com>
2025-06-15 11:27:42 +0300

    Added: Table top filters

--- resources/views/social_hub.blade.php
+++ resources/views/social_hub.blade.php
@@ -8,7 +8,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Social Hub</title>
+    <title>{{ __('landing.Social Hub') }} | {{ config('app.name') }}</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <script src="https://unpkg.com/@phosphor-icons/web"></script>
 
