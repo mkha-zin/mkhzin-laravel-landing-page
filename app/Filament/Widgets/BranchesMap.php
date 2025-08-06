e24540015291717229d15eb38427335eb36f8361
YousefAlamir <uoshy2016@gmail.com>
2025-08-06 18:10:55 +0300

    Added: branches map widget in admin dashboard

--- app/Filament/Widgets/BranchesMap.php
+++ app/Filament/Widgets/BranchesMap.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Widgets;
 
 use App\Models\Branch;
+use Illuminate\Support\Facades\App;
 use Webbingbrasil\FilamentMaps\Actions;
 use Webbingbrasil\FilamentMaps\Concerns\HasDarkModeTiles;
 use Webbingbrasil\FilamentMaps\Marker;
@@ -11,28 +12,22 @@ use Webbingbrasil\FilamentMaps\Widgets\MapWidget;
 class BranchesMap extends MapWidget
 {
     use HasDarkModeTiles;
-    protected int | string | array $columnSpan = 2;
+
+    protected int|string|array $columnSpan = 2;
 
     protected bool $hasBorder = false;
-
-    protected function setUp(): void
-    {
-        $this
-            ->rounded();
-
-    }
 
     public function getMarkers(): array
     {
         $branches = Branch::all();
         $markers = [];
 
-        if ( !empty($branches) ) {
+        if (!empty($branches)) {
             foreach ($branches as $branch) {
                 $markers[] = Marker::make('pos' . $branch->id)
                     ->lat($branch->latitude)
                     ->lng($branch->longitude)
-                    ->popup($branch->name);
+                    ->popup(App::currentLocale() == 'ar' ? $branch->name_ar : $branch->name_en);
             }
             return $markers;
         }
@@ -46,5 +41,12 @@ class BranchesMap extends MapWidget
             Actions\ZoomAction::make(),
             Actions\CenterMapAction::make()->zoom(3),
         ];
+    }
+
+    protected function setUp(): void
+    {
+        $this
+            ->rounded();
+
     }
 }
