d43e41cfcd95bf313652bc19858c972518cd8a59
YousefAlamir <uoshy2016@gmail.com>
2025-08-30 11:07:55 +0300

    Added: Google Analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -262,6 +262,16 @@
     </style>
 
 
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-JHRLHQREYM"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-JHRLHQREYM');
+    </script>
+    {{--Google Console--}}
     <meta name="google-site-verification" content="gOAvYgjJkpiE7MXHMfifJ_lGGLHnpJwDBjxU2o4TQI4"/>
 
     <!-- Snap Pixel Code -->
